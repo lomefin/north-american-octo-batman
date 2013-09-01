2900658e63ddc01e58bdbb4529fbb61703c77df4
Jose Tomas Robles Hahn <jtrh@jtrh.example.com>
2013-09-01 00:28:09 -0400

    Web Epic Python Incredible Code using Bottle microframework.

---
+++ python/wepic.py
@@ -0,0 +1,34 @@
+import bottle as btl
+
+import epic
+
+
+app = btl.Bottle()
+
+
+@app.route('/', method='GET')
+def index():
+	return 'Hello. My Name is Bottle and I am EPIC!!! because I run on Python'
+
+
+@app.route('/gpsredis', method='POST')
+def create_redis():
+	redisconn = epic.init_redis()
+
+	epic.process_redis(btl.request.body.read(), redisconn)
+
+
+@app.route('/gpspg', method='POST')
+def create_pg():
+	pgconn = epic.init_pg()
+	pgcur = pgconn.cursor()
+	epic.process_pg(btl.request.body.read(), pgcur)
+	pgcur.close()
+	pgconn.commit()
+	pgconn.close()
+
+
+if __name__ == "__main__":
+	app.run(host='localhost', port=8080, debug=False, reloader=False)
+else:
+	application = app
