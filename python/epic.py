bdd1ae6e474b4e1e5036a52771b93f75d8fc2e98
Jose Tomas Robles Hahn <jtrh@jtrh.example.com>
2013-09-01 00:27:26 -0400

    Improved code; added option to record stats into a CSV file

--- python/epic.py
+++ python/epic.py
@@ -2,14 +2,18 @@ import time
 import datetime
 import random
 import json
+import os
+import csv
 
 import psycopg2
 import redis
 import pytz
 
 
-pgconn = None
-pgcur = None
+PG_SERVER = os.environ['PG_SERVER']
+PG_USER = os.environ['PG_USER']
+PG_PASS = os.environ['PG_PASS']
+PG_DB = os.environ['PG_DB']
 PG_INSERT_QUERY = """INSERT INTO epic.incoming
 (name, position, time_generated)
 VALUES (%s, %s, CAST(%s as timestamp with time zone))"""
@@ -21,64 +25,49 @@ time_generated timestamp with time zone not null,
 primary key (name, position, time_generated)
 )"""
 
-redisconn = None
+
+REDIS_SERVER = os.environ['REDIS_SERVER']
+REDIS_PORT = 6379 if 'REDIS_PORT' not in os.environ else os.environ['REDIS_PORT']
+REDIS_DB = os.environ['REDIS_DB']
 REDISQUEUE = 'incoming'
 
-EXAMPLE_DATA = json.dumps(
-	{'name': 'pablogps', 'position': '666, -666', 'time_generated': '2013-08-30T22:33:44-0400'}
-)
 
-
-def initialize():
-	pgconn = psycopg2.connect(
-		host='localhost',
-		user='epic', password='test',
-		database='testdb'
+def init_pg():
+	return psycopg2.connect(
+		host=PG_SERVER,
+		user=PG_USER, password=PG_PASS,
+		database=PG_DB
 	)
+
+
+def init_redis():
 	redispool = redis.ConnectionPool(
-		host='localhost', db=13
+		host=REDIS_SERVER, port=REDIS_PORT, db=REDIS_DB
 	)
-	redisconn = redis.Redis(connection_pool=redispool)
-	return pgconn, redisconn
+	return redis.Redis(connection_pool=redispool)
 
 
-def process_redis(data):
-	redisconn.lpush(REDISQUEUE, data)
+def process_redis(data, p):
+	p.lpush(REDISQUEUE, data)
 
 
-def process_pg(data):
+def process_pg(data, p):
 	data = json.loads(data)
-	pgcur.execute(PG_INSERT_QUERY, (data['name'], data['position'], data['time_generated']))
-
-
-def example():
-	pgconn, redisconn = initialize()
-	pgcur = pgconn.cursor()
-
-	t_before = time.time()
-	process_redis(EXAMPLE_DATA)
-	t_after = time.time()
-	t_redis = t_after - t_before
-
-	t_before = time.time()
-	process_pg(EXAMPLE_DATA)
-	t_after = time.time()
-	t_pg = t_after - t_before
-
-	print t_redis, t_pg
+	p.execute(PG_INSERT_QUERY, (data['name'], data['position'], data['time_generated']))
 
 
 def attack(n):
-	global pgconn, redisconn, pgcur
-	#pgconn, redisconn = initialize()
+	#pgconn = init_pg()
+	#redisconn = init_redis()
 	#pgcur = pgconn.cursor()
 
 	time_acc_redis = 0
 	time_acc_pg = 0
 
 	for i in xrange(n):
-		pgconn, redisconn = initialize()
+		pgconn = init_pg()
 		pgcur = pgconn.cursor()
+		redisconn = init_redis()
 		data = json.dumps(
 			{
 				'name': 'pablogps',
@@ -87,13 +76,13 @@ def attack(n):
 			}
 		)
 		t_before = time.time()
-		process_redis(data)
+		process_redis(data, redisconn)
 		t_after = time.time()
 		t_redis = t_after - t_before
 		time_acc_redis += t_redis
 
 		t_before = time.time()
-		process_pg(data)
+		process_pg(data, pgcur)
 		pgconn.commit()
 		t_after = time.time()
 		t_pg = t_after - t_before
@@ -103,9 +92,24 @@ def attack(n):
 	#pgconn.commit()
 	#pgconn.close()
 
-	print 'n={n}, redis={t_redis}, pg={t_pg}, diff={d}/{f}, champion={ch}'.format(
+	print 'n={n}, redis={t_redis}, pg={t_pg}, diff={d} | {f}, champion={ch}'.format(
 		n=n,
 		t_redis=time_acc_redis, t_pg=time_acc_pg, d=time_acc_redis - time_acc_pg,
 		f=time_acc_redis / time_acc_pg,
 		ch='Redis' if time_acc_redis - time_acc_pg < 0 else 'PG'
 	)
+	return n, time_acc_redis, time_acc_pg, time_acc_redis - time_acc_pg, time_acc_redis/time_acc_pg
+
+def stats_csv(filename='./epic.csv', start=500, inc=500, end=50000):
+	print 'Beginning stats gathering... Filename is {fn}'.format(fn=filename)
+	with open(filename, 'wb') as f:
+		csvw = csv.writer(f)
+		csvw.writerow(['Rows', 'pg_time', 'redis_time'])
+		nn = xrange(start, end + inc, inc)
+		for n in nn:
+			r = attack(n)
+			csvw.writerow([r[0], r[2], r[1]])
+
+
+if __name__ == '__main__':
+	stats_csv()
