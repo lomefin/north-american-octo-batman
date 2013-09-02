61d73dba1a228cc67eff216b2cf2cccaa54289fc
Leonardo Luarte González <leonardo@luarte.net>
2013-09-02 12:36:20 -0400

    Configuration for NginX

--- ruby/web.rb
+++ ruby/web.rb
@@ -1,20 +1,25 @@
-require 'sinatra'
+require "rubygems"
+require "sinatra/base"
 load 'processor.rb'
 
+class Epic < Sinatra::Base
 
+  get '/' do
+    'Hello, nginx and unicorn!'
+  end
+  post '/' do
+    params.inspect
 
-get '/' do
-  puts params
+  end
+
+  post '/to_redis' do
+    processor = Processor.new
+    processor.process_redis(params[:data]) 
+  end
+
+  post '/to_pg' do
+    processor = Processor.new
+    processor.process_postgres(params[:data])
+  end
 
 end
-
-get '/to_redis' do
-  processor = Processor()
-  processor.process_redis(params[:data]) 
-end
-
-get '/to_pg' do
-  processor = Processor()
-  processor.process_postgres(params[:data])
-end
-
