ab8c2d990383486ebdbddf235b2cec63e1d54dbb
Jose Tomas Robles Hahn <jtrh@jtrh.example.com>
2013-09-09 20:09:12 -0400

    Fixed redis connection initialization

--- python/epic.py
+++ python/epic.py
@@ -13,8 +13,7 @@ REDISQUEUE = 'incoming'
 
 def init_redis():
 	redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-	redis = redis.from_url(redis_url)
-	return redis
+	return redis.from_url(redis_url)
 
 
 def process_redis(data, p):
